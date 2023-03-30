189aa713e38ebffc8cfd0b34bee4e3a3ec81bd5e
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-03-30 08:27:05 +0530

    Add files via upload
    Program to print increasing star.

---
+++ increasingstar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i,j,num,k,temp;
+	printf("enter the number");
+	scanf("%d",&num);
+	temp=num;
+	for(i=1;i<=num;i++)
+	{
+		for(j=1;j<=temp;j++)
+		{
+			printf(" ");
+		}
+		temp--;
+		printf("\n");
+		for(k=1;k<=i;k++)
+		{
+			printf("*");
+	    }
+	}
+}
